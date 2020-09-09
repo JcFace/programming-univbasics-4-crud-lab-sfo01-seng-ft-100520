cc72ec98ce3eb7b66bd4c7196b3979561a1d8ecd
Jesse Vaughn <jcv221@gmail.com>
2020-09-09 00:09:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,33 +1,38 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = ["Marcus", "Jesse", "Jordan", "Taylor"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I am"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = 
 end
 
 def retrieve_last_element_from_array(array)
